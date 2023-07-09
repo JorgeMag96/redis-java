b7223baa6b73e5a651e4796e73e53daac370a916
JorgeMag96 <jorge.vi96@gmail.com>
2023-07-09 09:15:38 -0600

    PING command

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,41 +1,51 @@
-import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Main {
-  public static void main(String[] args){
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    System.out.println("Logs from your program will appear here!");
+
+    private static final String PING_RESPONSE = "PONG";
+
+    public static void main(String[] args) {
+        // You can use print statements as follows for debugging, they'll be visible when running tests.
+        System.out.println("Logs from your program will appear here!");
 
         ServerSocket serverSocket = null;
         Socket clientSocket = null;
         int port = 6379;
         try {
-          serverSocket = new ServerSocket(port);
-          serverSocket.setReuseAddress(true);
-          // Wait for connection from client.
-          clientSocket = serverSocket.accept();
-            // Get input and output streams
-            DataInputStream in = new DataInputStream(clientSocket.getInputStream());
-            // Read the message from the client and reply
-            String clientMessage = in.readUTF();
-            System.out.println("Message from client: " + clientMessage);
+            serverSocket = new ServerSocket(port);
+            serverSocket.setReuseAddress(true);
+            // Wait for connection from client.
+            clientSocket = serverSocket.accept();
 
-            // Respond with a message
-            DataOutputStream out = new DataOutputStream(clientSocket.getOutputStream());
-            out.writeUTF("+PONG\r\n");
-        } catch (IOException e) {
-          System.out.println("IOException: " + e.getMessage());
-        } finally {
-          try {
-            if (clientSocket != null) {
-              clientSocket.close();
+            byte[] data = new byte[100];
+            int size = clientSocket.getInputStream().read(data);
+
+            String command = new String(data, 0, size);
+
+            if(command.startsWith("PING")) {
+                String argument = command.substring(4).trim();
+                System.out.println(String.format("Received command: PING %s", argument));
+
+                DataOutputStream out = new DataOutputStream(clientSocket.getOutputStream());
+                if(argument.length() > 0) {
+                    out.writeBytes(argument);
+                } else {
+                    out.writeBytes(PING_RESPONSE);
+                }
             }
-          } catch (IOException e) {
-            System.out.println("IOException: " + e.getMessage());
-          }
+        } catch (IOException e) {
+            System.out.println("IOException: " + e.getMessage() + ", " + e.getStackTrace()[0].toString());
+        } finally {
+            try {
+                if (clientSocket != null) {
+                    clientSocket.close();
+                }
+            } catch (IOException e) {
+                System.out.println("IOException: " + e.getMessage());
+            }
         }
-  }
+    }
 }
